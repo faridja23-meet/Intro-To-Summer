afb68af67052fa66b307ce207d83407e881eea14
faridja23-meet <109908216+faridja23-meet@users.noreply.github.com>
2022-07-24 18:00:01 +0300

    Create Lesson1.py

---
+++ Lesson1.py
@@ -0,0 +1,18 @@
+print("Hello World")
+# Farid
+list1 = [200, 'cs', 14]
+print(list1)
+list1.append("hello")
+print(list1)
+print(len(list1))
+dict1 = {"a": 1,"b": 2, "c" : 3}
+print(dict1)
+if "d" in dict1:
+	print("true")
+else:
+	print("false")
+
+print(dict1.keys())
+def create_student(name,year,gender):
+	student = {"Name": "farid", "Year": 2006,"Gender": "Male"}
+	Print(student)
